f0ddcb709e10fb6ec7c6ee1eeb5c3382270bf459
Ignacio <ignacioisas8@gmail.com>
2024-04-23 22:36:23 +0100

    navbar styled

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,19 +3,17 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-    <div className="navbar bg-base-100">
-        <div className="flex-none">
-            <button className="btn btn-square btn-ghost">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
-            </button>
-        </div>
+    <div className="navbar bg-slate-800 z-10">
         <div className="flex-1">
             <a className="btn btn-ghost text-xl"><h3>Pokedex</h3></a>
         </div>
         <div className="flex-none">
-            <button className="btn btn-square btn-ghost">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>
-            </button>
+        <div className="flex-none">
+            <ul className="menu menu-horizontal px-1">
+                <h2><li><a>About</a></li></h2>
+                <h2><li><a>Contact</a></li></h2>
+            </ul>
+        </div>
         </div>
         </div>
   );
