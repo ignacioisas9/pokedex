087a31b17bf97d9e98d297af8e76f035b97b0ec9
Ignacio <ignacioisas8@gmail.com>
2024-04-27 11:30:45 +0100

    style

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,9 +8,9 @@ const Card = ({ pokemon }) => {
     + name.slice(1)
 
   return (
-    <div className="bg-white shadow-md rounded-lg overflow-hidden">
+    <div className="bg-emerald-100 shadow-md rounded-lg overflow-hidden">
       <div className="relative">
-        <img src={img} alt={capitalizedName} className="w-full h-48 object-fit p-4" />
+        <img src={img} alt={capitalizedName} className="w-full h-80 object-fit p-4" />
         <div className="absolute top-0 right-0 m-2">
           <button className="p-1 text-gray-800 hover:text-yellow-500">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -19,11 +19,11 @@ const Card = ({ pokemon }) => {
           </button>
         </div>
       </div>
-      <div className="p-4">
-        <h6 className="text-lg font-bold">{capitalizedName}</h6>
-      </div>
-      <div className="bg-gray-100 p-4">
-        <span className="text-gray-600"><i className="fas fa-eye"></i> {id}</span>
+      <div className="bg-emerald-400 p-1 flex justify-center">
+        {/* <span className="text-gray-600"><i className="fas fa-eye"></i> {id}</span> */}
+        <div className="p-2 justify-center">
+          <h6 className="text-lg font-bold text-slate-700">{capitalizedName}</h6>
+        </div>
       </div>
     </div>
   );
